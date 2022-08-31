005b165ddbc166147cf411193fdcc64e42bd04b0
mausamik-iquadra <111846606+mausamik-iquadra@users.noreply.github.com>
2022-08-31 15:34:26 +0530

    updated

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-#code by Mausami 
+#Test cases for username testing 
 
-#Test cases for account creation
-
-#TEST CASES
+#TEST CASE 1 :
 ## if account already exists -- no entry added 
 ## login if account exists
 ## if account doesn't exist -- signup (before sign up check for all validation cases )
@@ -13,13 +11,14 @@
 ## Lower ## Upper ## len > 6 ## Digits 
 
 
+from urllib.parse import uses_relative
 import mysql.connector as sql
 from mysql.connector import Error
 from mysql.connector import errorcode
 import re
 
 try:
-#connect to mysql using the connect() with following parameters(own mysql credentials ) 
+#connect to mysql using the connect() with following parameters 
     connection = sql.connect(host='localhost',database='testpython',user='root', password='1234')
     print("Connection established")
 
@@ -35,27 +34,25 @@ except sql.Error as e :
 curs = connection.cursor()
 
 #function for signup 
-
-def signup(u,p):
-    curs.execute('Select * from users where name = %s  and password = %s ', (u,p))
+def signup(u,p, e):
+    curs.execute('Select * from users where name = %s  and password = %s  and email = %s', (u,p, e))
     acc = curs.fetchone()
-    
+
     # if account already exits 
     if acc:
         print('Account exists already')
     
     else :
     # if account doesn't exist --> create account 
-        query = ("insert into users(name  , password) values('{}','{}')".format(u,p))
+        query = ("insert into users(name , password, email) values('{}','{}','{}' )".format(u,p,e))
         curs.execute(query)
         connection.commit()
         print(" Created your account succesfully !")
 
 
 # function for signin / login 
-
-def signin(u,p):
-    curs.execute('Select * from users where name =%s and  password = %s ', (u,p))
+def signin(u,p,e):
+    curs.execute('Select * from users where name =%s and  password = %s and email = %s ', (u,p,e))
     account  = curs.fetchone()
     #check if account exists 
     if not account:
@@ -64,7 +61,7 @@ def signin(u,p):
         print("Logged in successfully ! ")
 
 # to validate username 
-class Authenticate(object):
+class AuthenticateUser(object):
     def __init__(self, username = ''):
         self.username = username
 
@@ -111,15 +108,32 @@ class Authenticate(object):
         else:
             pass
 
+regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b'
+def emailcheck(e):
+ 
+    # pass the regular expression
+    # and the string into the fullmatch() method
+    if(re.fullmatch(regex, e)):
+        print("Valid Email")
+        return True 
+ 
+    else:
+        print("Invalid Email")
+        return False
+
 
 # Driver code 
 print("Enter Username : ")
 user_name = input()
+
 print("Enter Password : ")
-pas = input() 
+pas = input()
 
-v = Authenticate(user_name)
+print("Enter email id : ")
+email = input()
 
+
+v = AuthenticateUser(user_name)
 
 print("Do you already have account ? (Y/ N) ")
 print("Account Yes ? Press 1 : ")
@@ -130,8 +144,8 @@ if inp == '1':
     signin(user_name,  pas)
 else :
     
-    if(v.validate() == True):
-        signup(user_name , pas)
+    if(v.validate() == True and emailcheck(email) == True):
+        signup(user_name , pas, email)
 
 
 
